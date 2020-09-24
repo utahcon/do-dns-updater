41cabe852d130fc5660a0970df8ef06b24c20519
Adam Barrett <adam.barrett.ext@bayer.com>
2020-09-23 18:22:20 -0600

    Changes

--- cmd/do-dns-updater/go.mod
+++ cmd/do-dns-updater/go.mod
@@ -2,4 +2,7 @@ module github.com/utahcon/do-dns-updater/cmd/do-dns-updater
 
 go 1.14
 
-require github.com/digitalocean/godo v1.44.0
+require (
+	github.com/digitalocean/godo v1.44.0
+	github.com/utahcon/regex v0.0.0-20200923194005-aef4968ae479
+)
--- cmd/do-dns-updater/main.go
+++ cmd/do-dns-updater/main.go
@@ -4,8 +4,26 @@ import (
 	"context"
 	"fmt"
 	"github.com/digitalocean/godo"
+	"github.com/utahcon/regex"
 	"net"
+	"strings"
 )
+
+/*
+
+IPv6 CIDR:
+^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))\/[0-9]{1,2}$
+
+IPv6:
+^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$
+
+IPv4:
+^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$
+
+IPV4 CIDR:
+^(?:[0-9]{1,3}\.){3}[0-9]{1,3}\/[0-9]{1,2}$
+
+*/
 
 type Configuration struct {
 	Key    string `yaml:"key"`
@@ -15,25 +33,29 @@ type Configuration struct {
 
 func main() {
 
-	client := godo.NewFromToken("396d2b3afce375a5f820a176704b88552d611a944498a3380e7cb4815677ed85")
+	config := &Configuration{
+		Key: "396d2b3afce375a5f820a176704b88552d611a944498a3380e7cb4815677ed85",
+		Domain: "utahcon.com",
+		Record: "home",
+	}
+
+	client := godo.NewFromToken(config.Key)
 
 	ctx := context.TODO()
 
-	ipv4Domains, response, err := client.Domains.RecordsByTypeAndName(ctx, "utahcon.com", "A", "home.utahcon.com", &godo.ListOptions{})
-	ipv6Domains, response, err := client.Domains.RecordsByTypeAndName(ctx, "utahcon.com", "AAAA", "home.utahcon.com", &godo.ListOptions{})
+	ipv4Domains, _, err := client.Domains.RecordsByTypeAndName(ctx, config.Domain, "A", strings.Join([]string{config.Record, config.Domain}, "."), &godo.ListOptions{})
 	if err != nil {
-		fmt.Printf("Error getting domains list: %v\n", err)
+		fmt.Printf("Error getting IPv4 domains list: %v\n", err)
 	}
 
-	fmt.Printf("Response: %v\n", response)
+	//ipv6Domains, _, err := client.Domains.RecordsByTypeAndName(ctx, config.Domain, "AAAA", strings.Join([]string{config.Record, config.Domain}, "."), &godo.ListOptions{})
+	//if err != nil {
+	//	fmt.Printf("Error getting IPv6 domains list: %v\n", err)
+	//}
 
-	for _, domain := range ipv4Domains {
-		fmt.Printf("Domain: %v\n", domain)
-	}
-
-	for _, domain := range ipv6Domains {
-		fmt.Printf("Domain: %v\n", domain)
-	}
+	//for _, domain := range ipv6Domains {
+	//	fmt.Printf("Domain: %v\n", domain)
+	//}
 
 	interfaces, err := net.Interfaces()
 	if err != nil {
@@ -45,31 +67,46 @@ func main() {
 		if err != nil {
 			fmt.Printf("error: %s\n", err)
 		}
-		for _, addr := range addresses {
-			isPrivate, err := CheckIpIsPrivate(addr)
+
+		for _, address := range addresses {
+			addr, _, err := net.ParseCIDR(address.String())
+			if err != nil {
+				return false, err
+			}
+
+			isPrivate, err := CheckIpIsPrivate(addr.String())
 			if err != nil {
 				fmt.Printf("error: %s\n", err)
 			}
-			if isPrivate {
-				drer := &godo.DomainRecordEditRequest{
-					Type: "A",
-					Name: "home.utahcon.com",
-					Data: addr.String(),
-					TTL:  300,
-				}
 
+			if !isPrivate {
+				if ipv4.Validate(addr.String()){
+					fmt.Println("IP Address is IPv4")
+					for _, domain := range ipv4Domains {
+						fmt.Printf("Domain: %v\n", domain)
+						drer := &godo.DomainRecordEditRequest{
+							Type: "A",
+							Name: config.Record,
+							Data: net.IP(addr,
+							TTL: 300,
+						}
+						fmt.Printf("Request: %v", drer)
+						newRecord, response, err := client.Domains.EditRecord(ctx, config.Domain, domain.ID, drer)
+						if err != nil {
+							fmt.Printf("New Record: %v\n", newRecord)
+							fmt.Printf("Response: %v\n", response)
+							fmt.Printf("Error updating record: %v\n", err)
+						}
+
+					}
+				}
 			}
 			fmt.Printf("%s is Private: %t\n", addr.String(), isPrivate)
 		}
 	}
 }
 
-func CheckIpIsPrivate(address net.Addr) (bool, error) {
-	addr, _, err := net.ParseCIDR(address.String())
-	if err != nil {
-		return false, err
-	}
-
+func CheckIpIsPrivate(address string) (bool, error) {
 	if net.ParseIP(addr.String()).IsLoopback() {
 		return true, nil
 	}
